2a4c383463970076e5088583e0661410b0afba4e
MMADI Ben Youssam <ben@mamba.local>
2022-12-08 18:31:05 +0100

    Le projet voyages

---
+++ script.js
@@ -0,0 +1,34 @@
+function lune(id){
+    document.getElementById(id).style.background = "url(img/ilenuit.gif) no-repeat fixed";
+    document.getElementById(id).style.backgroundSize = "cover";
+}
+function enleve(id){
+    document.getElementById(id).style.display = "none";
+}
+function mettre(id){
+    document.getElementById(id).style.display = "block";
+}
+function lune2(id){
+    document.getElementById(id).style.background = "url(img/ile.jpeg) no-repeat";
+    document.getElementById(id).style.backgroundSize = "cover";
+}
+function enleve2(id){
+    document.getElementById(id).style.display = "none";
+}
+function mettre2(id){
+    document.getElementById(id).style.display = "block";
+}
+
+// *******COMORES*******
+function cachebouton(id){
+    document.getElementById(id).style.display = "none";
+}
+function affiche_header_div(id){
+    document.getElementById(id).style.display = "block";
+}
+function bodynoflex(id){
+    document.getElementById(id).style.display = "block";
+    document.getElementById(id).style.justifyContent = '';
+    document.getElementById(id).style.backgroundColor = "#70b262";
+    document.getElementById(id).style.backgroundSize = 'cover';
+}
